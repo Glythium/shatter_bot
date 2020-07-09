66c38e0b40a1bfa1c4312ae80d052fb15401106f
Alex <alexdow1991@gmail.com>
2020-07-09 19:33:20 -0400

    Initial commit

---
+++ bot.py
@@ -0,0 +1,46 @@
+import os
+from twitchio.ext import commands
+
+class Bot(commands.Bot):
+
+    def __init__(self):
+        super().__init__(
+            irc_token=os.environ['TMI_TOKEN'],
+            client_id=os.environ['CLIENT_ID'],
+            nick=os.environ['BOT_NICK'],
+            prefix=os.environ['BOT_PREFIX'],
+            initial_channels=[os.environ['CHANNEL']]
+        )
+
+    async def event_ready(self):
+        'Called once when the bot goes online.'
+        print(f"{os.environ['BOT_NICK']} is in {os.environ['CHANNEL']}'s chat!")
+
+
+    async def event_message(self, ctx):
+        'Runs every time a message is sent in chat.'
+
+        # make sure the bot ignores itself and the streamer
+        if ctx.author.name.lower() == os.environ['BOT_NICK'].lower():
+            return
+        
+        await bot.handle_commands(ctx)
+
+        if 'hello' in ctx.content.lower():
+            await ctx.channel.send(f"Hi, @{ctx.author.name}!")
+        
+
+############################## Commands #######################################
+    @commands.command(name='help')
+    async def help(self, ctx):
+        await ctx.send(f"There's no help for you, @{ctx.author.name}")
+
+
+    @commands.command(name='clip')
+    async def clip(self, ctx):
+        await ctx.send(f"Coming Soon!")
+
+
+if __name__ == "__main__":
+    bot = Bot()
+    bot.run()
