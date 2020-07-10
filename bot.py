a9aa32fb5934e6477ac49e5c6b426925dc0486e0
Alex <alexdow1991@gmail.com>
2020-07-10 14:56:52 -0400

    Updated personality

--- bot.py
+++ bot.py
@@ -10,17 +10,18 @@ class CoolDownManager():
         self.start = time.time()
         self.multiplier = {
             "sec": 1,
-            "second": 1,
-            "seconds": 1,
             "min": 60,
-            "minute": 60,
-            "minutes": 60,
-            "hour": 3600,
-            "hours": 3600
+            "hour": 3600
         }
 
     def updateCooldown(self, number, unit):
         try:
+            if unit in ["second", "seconds"]:
+                unit = "sec"
+            elif unit in ["minute", "minutes"]:
+                unit = "min"
+            elif unit == "hours":
+                unit = "hour"
             new_time = number * self.multiplier[unit]
         except KeyError:
             return None
@@ -51,29 +52,37 @@ class Bot(commands.Bot):
             "Part-time bot, full-time edgelord.",
             "Don't hurt yourself, kid.",
             "It's time to shatter and splatter.",
-            "One more for the road."
+            "It's time to D-D-D-DUEL!",
+            "Can you smell what this bot is cookin'?",
+            "Simpin' ain't easy..."
         ]
         self.quips = [
             "So...you have chosen death", 
             "I may not have eyes, but I know what a scrub looks like",
-            "4/5 dentists recommend you stop trying so hard",
-            "I'm cookin' up knuckle sandwiches with extra beef"
+            "4/5 dentists recommend you stop failing so hard",
+            "I'm cookin' up knuckle sandwiches with extra beef",
+            "In these uncertain times, we can all count on you being trash",
+            "Everyone here is dumber for having heard you speak",
         ]
         self.thanks = [
-            "Thank you kindly",
+            "Thank you kind stranger",
             "I aim to please",
             "You're alright",
             "All in a day's work",
-            "uwu"
+            "uwu",
+            "Hail to the king,",
         ]
         self.jeers = [
             "You just made the list",
             "Bots have feelings too",
             "Oh, come on",
             "I'll ignore that one",
+            "Bite me",
+            "Oooo, yes...harder"
         ]
         self.iceBreakers = [
             "What's your favorite videogame",
+            "Which Pokemon would you eat, if you could"
         ]
         self.goodWords = [
             "like",
@@ -89,6 +98,7 @@ class Bot(commands.Bot):
         self.badWords = [
             "bad",
             "worse",
+            "worst",
             "terrible",
             "horrible",
             "awful",
@@ -100,8 +110,8 @@ class Bot(commands.Bot):
 
 ################################ Actual Functions #############################
     def randShoutout(self, username):
-        'Randomly tacks on the @username'
-        name = random.choice(["", '@' + username])
+        'Randomly tacks on the @username, nothing, or meatbag'
+        name = random.choice(["", '@' + username, "meatbag"])
         return name
 
     async def event_ready(self):
@@ -114,14 +124,17 @@ class Bot(commands.Bot):
         if ctx.author.name.lower() == os.environ['BOT_NICK'].lower():
             return
         
+        # Always handle commands as they come in.
         await bot.handle_commands(ctx)
 
+        # Don't interact with chat if you're on cool down.
         if self.cooldownMgr.isCool():
             if any(word in ctx.content.lower() for word in self.goodWords):
                 await ctx.channel.send(f"{random.choice(self.catchPhrases)}")
             elif any(word in ctx.content.lower() for word in self.badWords):
                 await ctx.channel.send(f"{random.choice(self.quips)} {self.randShoutout(ctx.author.name)}")
 
+        # Always say hi and bye if prompted.
         if 'hello' in ctx.content.lower():
             await ctx.channel.send(f"Hi, @{ctx.author.name}!")
         elif 'bye' in ctx.content.lower():
@@ -131,11 +144,12 @@ class Bot(commands.Bot):
 ############################## Commands #######################################
     @commands.command(name='help')
     async def help(self, ctx):
-        # Usage statement???
+        'Maybe could be a usage statement? WIP'
         await ctx.send(f"I can't help you, @{ctx.author.name}")
 
     @commands.command(name='roll')
     async def roll(self, ctx):
+        'Roll a dice. The command arg is the sides of the die.'
         try:
             args = ctx.content.lower().split()
             number = int(args[1])
@@ -145,17 +159,17 @@ class Bot(commands.Bot):
     
     @commands.command(name="goodBot")
     async def goodBot(self, ctx):
-        # Bots need lovin'
+        'Post a special message to chat'
         await ctx.send(f"{random.choice(self.thanks)} {self.randShoutout(ctx.author.name)}")
     
     @commands.command(name="badBot")
     async def badBot(self, ctx):
-        # Bots need hatin'
+        'Post a special message to chat'
         await ctx.send(f"{random.choice(self.jeers)} {self.randShoutout(ctx.author.name)}")
     
     @commands.command(name='cooldown')
     async def setCooldown(self, ctx):
-        # Set a new cooldown timer
+        'Set a new cooldown timer. The command args are number and units.'
         if ctx.author.is_mod:
             args = ctx.content.lower().split()
             number = int(args[1])
