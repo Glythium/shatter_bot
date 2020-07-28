0c2bf41b24afb7af6d04b9270a2f25b44c270813
Alex <alexdow1991@gmail.com>
2020-07-28 19:40:19 -0400

    Added Personality() class and modules

--- bot.py
+++ bot.py
@@ -1,7 +1,104 @@
+import json
 import os
 import random
 import time
 from twitchio.ext import commands
+
+
+######################### Personality #########################################
+class Personality():
+    def __init__(self):
+        self.goodWords = [
+            "love ",
+            "good ",
+            "great ",
+            "awesome ",
+            "best ",
+            "right ",
+            "cheer ",
+            "chat "
+        ]
+        self.badWords = [
+            "bad ",
+            "worse ",
+            "worst ",
+            "terrible ",
+            "horrible ",
+            "awful ",
+            "hate ",
+            "suck ",
+            "stink ",
+        ]
+        self.catchPhrases = [
+            "Part-time bot, full-time edgelord.",
+            "Don't hurt yourself, kid.",
+            "It's time to shatter and splatter.",
+            "It's time to D-D-D-DUEL!",
+            "Can you smell what this bot is cookin'?",
+        ]
+        self.quips = [
+            "So...you have chosen death", 
+            "I may not have eyes, but I know what a scrub looks like",
+            "4/5 dentists recommend you stop failing so hard",
+            "I'm cookin' up knuckle sandwiches with extra beef",
+            "In these uncertain times, we can all count on you being trash",
+        ]
+        self.thanks = [
+            "Thank you kind stranger",
+            "I aim to please",
+            "You're alright",
+            "All in a day's work",
+            "UwU",
+            "Hail to the king",
+        ]
+        self.jeers = [
+            "You just made the list",
+            "Bots have feelings too",
+            "Oh, come on",
+            "I'll ignore that one",
+            "Bite me",
+            "Oooo, yes...harder"
+        ]
+        self.noPerms = [
+            "You're not my supervisor",
+            "Whatever, I do what I want",
+            "Not right now, I have a headache",
+            "I can't let you do that",
+            "You don't have enough badges to train me",
+            "No, no, a thousand times no"
+        ]
+
+
+class MarioPersonality(Personality):
+    def __init__(self):
+        super().__init__()
+        self.marioTime = [
+            "It's a me, shatter_bot!",
+            "It's a Mario time!",
+            "I'm ready for some Mario Maker",
+            "Mama Luigi!",
+            "Kill the Yoshis!"
+        ]
+
+
+class SoulsPersonality(Personality):
+    def __init__(self):
+        super().__init__()
+        self.soulsTime = [
+            "Use !showDeaths to see the total deaths, good luck!"
+        ]
+
+        self.thanks += [
+            "Praise the Sun",
+            "Humanity Restored",
+            "You really light my bonfire",
+        ]
+
+        self.quips += [
+            "Prepare to die",
+            "Git gud",
+            "You are not prepared",
+        ]
 
 
 class CoolDownManager():
@@ -36,7 +133,6 @@ class CoolDownManager():
 
 
 class Bot(commands.Bot):
-
     def __init__(self):
         super().__init__(
             irc_token=os.environ['TMI_TOKEN'],
@@ -46,83 +142,18 @@ class Bot(commands.Bot):
             initial_channels=[os.environ['CHANNEL']]
         )
         self.cooldownMgr = CoolDownManager(5)
+        self.vibe = Personality()
         self.levelQueue = []
         self.canAddLevels = True
         self.isPlayingMario = False
-
-######################### Personality #########################################
-        self.catchPhrases = [
-            "Part-time bot, full-time edgelord.",
-            "Don't hurt yourself, kid.",
-            "It's time to shatter and splatter.",
-            "It's time to D-D-D-DUEL!",
-            "Can you smell what this bot is cookin'?",
-            "Simpin' ain't easy..."
-        ]
-        self.quips = [
-            "So...you have chosen death", 
-            "I may not have eyes, but I know what a scrub looks like",
-            "4/5 dentists recommend you stop failing so hard",
-            "I'm cookin' up knuckle sandwiches with extra beef",
-            "In these uncertain times, we can all count on you being trash",
-            "Everyone here is dumber for having heard you speak",
-        ]
-        self.thanks = [
-            "Thank you kind stranger",
-            "I aim to please",
-            "You're alright",
-            "All in a day's work",
-            "UwU",
-            "Hail to the king",
-        ]
-        self.jeers = [
-            "You just made the list",
-            "Bots have feelings too",
-            "Oh, come on",
-            "I'll ignore that one",
-            "Bite me",
-            "Oooo, yes...harder"
-        ]
-        self.noPerms = [
-            "You're not my supervisor",
-            "Whatever, I do what I want",
-            "Not right now, I have a headache",
-            "I can't let you do that",
-            "You don't have enough badges to train me",
-            "No, no, a thousand times no"
-        ]
-        self.iceBreakers = [
-            "What's your favorite videogame",
-            "Which Pokemon would you eat, if you could"
-        ]
-        self.marioTime = [
-            "It's a me, shatter_bot!",
-            "It's a Mario time!",
-            "I'm ready for some Mario Maker?",
-            "Mama Luigi!",
-            "Kill the Yoshis!"
-        ]
-        self.goodWords = [
-            "love ",
-            "good ",
-            "great ",
-            "awesome ",
-            "best ",
-            "right ",
-            "cheer ",
-            "chat "
-        ]
-        self.badWords = [
-            "bad ",
-            "worse ",
-            "worst ",
-            "terrible ",
-            "horrible ",
-            "awful ",
-            "hate ",
-            "suck ",
-            "stink ",
-        ]
+        self.isPlayingSouls = False
+        self.state = dict()
+        try:
+            with open("state.json", "r+") as fp:
+                self.state = json.load(fp)
+        except:
+            # Seed the state with all used values
+            self.state["deaths"] = 0
 
 ################################ Actual Functions #############################
     def randShoutout(self, username):
@@ -146,10 +177,10 @@ class Bot(commands.Bot):
 
         # Don't interact with chat if you're on cool down.
         if self.cooldownMgr.isCool():
-            if any(word in ctx.content.lower() for word in self.goodWords):
-                await ctx.channel.send(f"{random.choice(self.catchPhrases)}")
-            elif any(word in ctx.content.lower() for word in self.badWords):
-                await ctx.channel.send(f"{random.choice(self.quips)} {self.randShoutout(ctx.author.name)}")
+            if any(word in ctx.content.lower() for word in self.vibe.goodWords):
+                await ctx.channel.send(f"{random.choice(self.vibe.catchPhrases)}")
+            elif any(word in ctx.content.lower() for word in self.vibe.badWords):
+                await ctx.channel.send(f"{random.choice(self.vibe.quips)} {self.randShoutout(ctx.author.name)}")
 
         # Always say hi and bye if prompted.
         # if any(word in ctx.content.lower() for word in ("hello ", "hi ", "heya ")):
@@ -177,9 +208,11 @@ class Bot(commands.Bot):
     @commands.command(name="mm")
     async def marioTime(self, ctx):
         'Turns on or off the ability to use the Mario Maker level queue commands'
-        self.isPlayingMario = not self.isPlayingMario
-        if self.isPlayingMario:
-            await ctx.send(f"{random.choice(self.marioTime)}")
+        if ctx.author.is_mod:
+            self.isPlayingMario = not self.isPlayingMario
+            if self.isPlayingMario:
+                self.vibe = MarioPersonality()
+                await ctx.send(f"{random.choice(self.vibe.marioTime)}")
 
     @commands.command(name="modLevels")
     async def modLevels(self, ctx):
@@ -227,12 +260,12 @@ class Bot(commands.Bot):
     @commands.command(name="goodBot")
     async def goodBot(self, ctx):
         'Post a special message to chat'
-        await ctx.send(f"{random.choice(self.thanks)} {self.randShoutout(ctx.author.name)}")
+        await ctx.send(f"{random.choice(self.vibe.thanks)} {self.randShoutout(ctx.author.name)}")
     
     @commands.command(name="badBot")
     async def badBot(self, ctx):
         'Post a special message to chat'
-        await ctx.send(f"{random.choice(self.jeers)} {self.randShoutout(ctx.author.name)}")
+        await ctx.send(f"{random.choice(self.vibe.jeers)} {self.randShoutout(ctx.author.name)}")
     
     @commands.command(name='cooldown')
     async def setCooldown(self, ctx):
@@ -248,6 +281,34 @@ class Bot(commands.Bot):
                 await ctx.send(f"Set cooldown for {new_time}!")
         else:
             await self._noPerms(ctx)
+    
+    @commands.command(name='addDeath')
+    async def addDeath(self, ctx):
+        'Another one bites the dust'
+        if ctx.author.is_mod:
+            self.state["deaths"] += 1
+            await ctx.send(f"Total Deaths: {self.state['deaths']}")
+    
+    @commands.command(name='showDeaths')
+    async def showDeaths(self, ctx):
+        'Shows the number of deaths'
+        await ctx.send(f"Total Deaths: {self.state['deaths']}")
+    
+    @commands.command(name='clearDeaths')
+    async def clearDeaths(self, ctx):
+        'Resets the number of deaths'
+        if ctx.author.is_mod:
+            self.state["deaths"] = 0
+            await ctx.send(f"Total deaths reset!")
+    
+    @commands.command(name='darkSouls')
+    async def soulsTime(self, ctx):
+        'Toggles the isPlayingSouls flag'
+        if ctx.author.is_mod:
+            self.isPlayingSouls = not self.isPlayingSouls
+            if self.isPlayingSouls:
+                self.vibe = SoulsPersonality()
+                await ctx.send(f"{random.choice(self.vibe.soulsTime)}")
 
 ########################### Misc. Methods #####################################
     async def _currentLevel(self, ctx):
@@ -259,10 +320,14 @@ class Bot(commands.Bot):
     
     async def _noPerms(self, ctx):
         'Called when a user does not have permissions to use a command'
-        await ctx.send(f"{random.choice(self.noPerms)} {self.randShoutout(ctx.author.name)}")
+        await ctx.send(f"{random.choice(self.vibe.noPerms)} {self.randShoutout(ctx.author.name)}")
 
 
 if __name__ == "__main__":
     random.seed()
     bot = Bot()
     bot.run()
+    print(f"Saving state...")
+    with open("state.json", "w+") as fp:
+        json.dump(bot.state, fp)
+        print(f"Saved!")
